d3eb5fcc927e370ac6a57f84ea2c22081bb418de
Chinedu N <chine_n@outlook.com>
2022-05-17 20:27:20 +0100

    initial commit

--- networking.tf
+++ networking.tf
@@ -31,6 +31,7 @@ resource "aws_instance" "main" {
     ami = each.value.ami
     instance_type = each.value.instance_type
     #subnet_id = each.value.subnet_id
+    # or
     subnet_id = each.value.type == "public" ? aws_subnet.main[0].id : aws_subnet.main[1].id
     vpc_security_group_ids = each.value.vpc_sec_grp_id
 
